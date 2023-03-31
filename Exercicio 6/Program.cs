d4022f6f05972f8ecf7a7de56f428de18bd08c21
merbach020202 <eduardo.brenn2006@gmail.com>
2023-03-31 09:14:42 -0300

    31/03

---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine($"Digite sua frequência em decimal (Ex: 85% = 0,85): ");
+float frequência = float.Parse(Console.ReadLine());
+
+if (frequência < 0.75F)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"Reprovado !");
+    Console.ResetColor();
+}
+
+Console.WriteLine($"Digite sua média");
+float media = float.Parse(Console.ReadLine());
+
+if (media <= 7 && media >= 3 && frequência >= 0.75)
+{
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Recuperação !");
+    Console.ResetColor();
+}
+else if (media > 7 && frequência > 0.75)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Aprovado!");
+    Console.ResetColor();
+}
+
+
